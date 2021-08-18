fa08c3386ade1bb2b3033d105861b469037ceae7
adamgft <66415638+adamgft@users.noreply.github.com>
2021-08-18 10:20:44 +0100

    added . for linux

--- scripts/2-environments/environments.sh
+++ scripts/2-environments/environments.sh
@@ -31,7 +31,7 @@ echo Checking out plan
 git checkout -b plan
 
 echo Copying needed build files
-cp -R ../terraform-example-foundation/2-environments/ .
+cp -R ../terraform-example-foundation/2-environments/. .
 cp ../terraform-example-foundation/build/cloudbuild-tf-* .
 cp ../terraform-example-foundation/build/tf-wrapper.sh .
 chmod 755 ./tf-wrapper.sh
