1288cccc71453483134a8a75402cc424682915d9
adamgft <66415638+adamgft@users.noreply.github.com>
2021-08-18 11:56:50 +0100

    bugfix terraform.auto.tfvars.json

--- scripts/2-environments/environments.sh
+++ scripts/2-environments/environments.sh
@@ -40,32 +40,19 @@ echo Removing unneeded variable file from current dir
 TF_EXAMPLE_VARS=./terraform.example.tfvars
 [ -f $TF_EXAMPLE_VARS ] && { echo "Removing unneeded terraform.example.tfvars file: $TF_EXAMPLE_VARS"; rm $TF_EXAMPLE_VARS; } || { echo "No terraform.example.tfvars file found"; }
 
-echo Removing unneeded variables from dev
-TF_EXAMPLE_VARS=./env/development/terraform.tfvars
-[ -f $TF_EXAMPLE_VARS ] && { echo "Removing unneeded terraform.example.tfvars file: $TF_EXAMPLE_VARS"; rm $TF_EXAMPLE_VARS; } || { echo "No terraform.example.tfvars file found"; }
-
-echo Removing unneeded variables from non-prod
-TF_EXAMPLE_VARS=./env/non-production/terraform.tfvars
-[ -f $TF_EXAMPLE_VARS ] && { echo "Removing unneeded terraform.example.tfvars file: $TF_EXAMPLE_VARS"; rm $TF_EXAMPLE_VARS; } || { echo "No terraform.example.tfvars file found"; }
-
-echo Removing unneeded variables from prod
-TF_EXAMPLE_VARS=./env/production/terraform.tfvars
-[ -f $TF_EXAMPLE_VARS ] && { echo "Removing unneeded terraform.example.tfvars file: $TF_EXAMPLE_VARS"; rm $TF_EXAMPLE_VARS; } || { echo "No terraform.example.tfvars file found"; }
-
-
 echo Copying in needed variables for dev
 TF_VARS=../../scripts/2-environments/terraform.auto.tfvars.json
-COPY_LOCATION=./env/development/.
+COPY_LOCATION=./env/development/
 [ -f $TF_VARS ] && { echo "Copying $TF_VARS to $COPY_LOCATION"; cp $TF_VARS $COPY_LOCATION; } || { echo "No $TF_VARS file found"; exit 1; }
 
 echo Copying in needed variables for non-prod
 TF_VARS=../../scripts/2-environments/terraform.auto.tfvars.json
-COPY_LOCATION=./env/non-production/.
+COPY_LOCATION=./env/non-production/
 [ -f $TF_VARS ] && { echo "Copying $TF_VARS to $COPY_LOCATION"; cp $TF_VARS $COPY_LOCATION; } || { echo "No $TF_VARS file found"; exit 1; }
 
 echo Copying in needed variables for prod
 TF_VARS=../../scripts/2-environments/terraform.auto.tfvars.json
-COPY_LOCATION=./env/production/.
+COPY_LOCATION=./env/production/
 [ -f $TF_VARS ] && { echo "Copying $TF_VARS to $COPY_LOCATION"; cp $TF_VARS $COPY_LOCATION; } || { echo "No $TF_VARS file found"; exit 1; }
 
 echo pushing plan
